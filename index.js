8a814b6976a3fe2d67095eb8ecd4834ea650f5cc
kamruzzaman-leeon <leeon.ewubd@gmail.com>
2023-11-04 13:40:31 +0600

    Handle client specific product data request, retrieve data from the server, and send it to the user.

--- index.js
+++ index.js
@@ -27,43 +27,64 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
         // connect to Atlas cluste & collection
-        const BrandShopCollection = client.db('BrandShopDB').collection('product')
+        const ProductCollection = client.db('BrandShopDB').collection('product')
+        const BrandCollection = client.db('BrandShopDB').collection('brand')
 
         //data create
         app.post('/product', async (req, res) => {
             const newProduct = req.body;
             console.log(newProduct);
-            const result = await BrandShopCollection.insertOne(newProduct);
+            const result = await ProductCollection.insertOne(newProduct);
             res.send(result);
 
         })
 
-          // data read
-    app.get('/product', async (req, res) => {
-        const cursor = BrandShopCollection.find()
-        const result = await cursor.toArray();
-        res.send(result);
-      })
+        // brand get from database
+        app.get('/brand', async (req, res) => {
+            const cursor = BrandCollection.find();
+            const result = await cursor.toArray()
+            res.send(result);
+        })
+
+        // data read
+        app.get('/product', async (req, res) => {
+            const cursor = ProductCollection.find()
+            const result = await cursor.toArray();
+            res.send(result);
+        })
 
         // data read for a specific brand
-        app.get('/product/:brand',async(req,res)=>{
-            const brand = req.params.brand;
-            const query = {brand: brand};
-            const result = await BrandShopCollection.find(query).toArray();
+        app.get('/product/:brand', async (req, res) => {
+            const encodeBrand = req.params.brand;
+            const brand = decodeURIComponent(encodeBrand);
+            const query = { brand: brand };
+            const result = await ProductCollection.find(query).toArray();
             res.send(result)
         })
 
+        // data read for a specific product
+        app.get('/productdetails/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await ProductCollection.findOne(query);
+            console.log(result)
+            res.send(result);
+        })
+   
 
 
 
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-        // Ensures that the client will close when you finish/error
-        // await client.close();
-    }
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+} finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+}
 }
 run().catch(console.dir);
 
